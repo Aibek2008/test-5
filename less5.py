7c22ade5dab116ea4c6d9abb0dfb1b45c977f6c8
Aibek <shadowold2008@gmail.com>
2024-12-26 16:25:46 +0600

    aibek5

---
+++ less5.py
@@ -0,0 +1,19 @@
+from abc import ABC,abstractmethod
+
+class Animal(ABC):
+    @abstractmethod
+    def make_sound(self):
+        pass
+
+class Dog(Animal):
+    def make_sound(self):
+        return 'Woof!'
+
+class Cat(Animal):
+    def make_sound(self):
+        return 'Meow!'
+
+# Использование
+animals = [Dog(),Cat()]
+for animal in animals:
+    print(animal.make_sound())
